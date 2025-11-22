c48605276f2e7f080f748e4f3fc71f4dfe6c8884
holy <814089524@qq.com>
2025-11-22 15:31:51 +0800

    创建导航栏和游戏地图

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import PkIndexView from '@/views/pk/PkIndexView.vue'
+import RanKlistIndexView from '@/views/ranklist/RanKlistIndexView.vue'
+import RecordIndexView from '@/views/record/RecordIndexView.vue'
+import UserBotIndexView from '@/views/user/bot/UserBotIndexView.vue'
+import NotFound from '@/views/error/NotFound.vue'
+import UserSettingsVies from '@/views/user/bot/settings/UserSettingsVies.vue'
 const routes = [
+  {
+    path:"/",
+    name:"home",
+    component:PkIndexView,
+  },
+  {
+    path:"/pk/",
+    name:"pk_index",
+    component:PkIndexView,
+  },
+  {
+    path:"/ranklist/",
+    name:"RanKlist_index",
+    component:RanKlistIndexView,
+  },
+  {
+    path:"/record/",
+    name:"record_index",
+    component:RecordIndexView,
+  },
+  {
+    path:"/user/bot/",
+    name:"userbot_index",
+    component:UserBotIndexView,
+  },
+  {
+    path:"/user/settings/",
+    name:"settings_index",
+    component: UserSettingsVies,
+  },
+  {
+    path:"/:CatchAll(.*)",
+    name:"NotFound_index",
+    component:NotFound,
+  },
 ]
 
 const router = createRouter({
