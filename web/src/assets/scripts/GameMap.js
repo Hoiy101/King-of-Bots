f2eb6061f18c2a9906036ae41e84b78245deb24a
holy <814089524@qq.com>
2025-11-28 09:10:13 +0800

    实现了蛇的动画和死亡逻辑

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,6 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
 import { Wall } from "./Wall";
-import { Snake } from "./Snake";
+import { Snake } from './Snake';
 export class GameMap extends AcGameObject{
     constructor(ctx,parent){
         super();
@@ -18,7 +18,8 @@ export class GameMap extends AcGameObject{
         this.snakes = [
             new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
             new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
-        ]
+        ];
+
     }
 
     check_connectivity(g, sx, sy, tx, ty){ // 判断左下角和右上角是否连通
@@ -86,7 +87,7 @@ export class GameMap extends AcGameObject{
         this.ctx.canvas.focus();
 
         const [snake0, snake1] = this.snakes;
-        this.ctx.canvas.addEventListener("keydown", e => {
+        this.ctx.canvas.addEventListener("keydown", e => { // 从键盘中读入方向，并且不会回头
             if(e.key === 'w') snake0.set_direction(0);
             else if(e.key === 'd') snake0.set_direction(1);
             else if(e.key === 's') snake0.set_direction(2);
@@ -122,14 +123,35 @@ export class GameMap extends AcGameObject{
     check_ready(){
         for(const snake of this.snakes){
             if(snake.status !== "idle") return false;
-            if(snake.status === -1) return false;
+            if(snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    check_valid(cell){
+        for(const wall of this.walls){
+            if(wall.r === cell.r && wall.c === cell.c){
+                return false;
+            }
+        }
+
+        for(const snake of this.snakes){
+            let k = snake.cells.length;
+            if(!snake.check_tall_increasing()){
+                k --;
+            }
+            for(let i = 0; i < k; i ++){
+                if(snake.cells[i].r === cell.r && snake.cells[i].c === cell.c){
+                    return false;
+                }
+            }
         }
         return true;
     }
 
     update(){
         this.update_size();
-        if(this.check_ready){
+        if(this.check_ready()){
             this.next_step();
         }
         this.render();
