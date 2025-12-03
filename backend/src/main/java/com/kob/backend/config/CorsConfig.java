ba1f377186130da5579e559038bde23f826bbf42
holy <814089524@qq.com>
2025-12-03 17:59:38 +0800

    重构后端代码

--- backend/src/main/java/com/kob/backend/config/CorsConfig.java
+++ backend/src/main/java/com/kob/backend/config/CorsConfig.java
@@ -41,5 +41,3 @@ public class CorsConfig implements Filter {
     public void destroy() {
     }
 }
-
-
--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,18 +3,18 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/pk/")
+@RequestMapping("/pk")
 public class BotInfoController {
 
-    @RequestMapping("gitbotinfo/")
-    public Map<String,String> gitbotinfo(){
-        Map<String,String> bot1 = new HashMap<>();
-        bot1.put("name", "tiger");
-        bot1.put("rating", "1500");
-        return bot1;
+    @RequestMapping("/getbotinfo/")
+    public List<String> getBotInfo() {
+        List<String> list = new LinkedList<>();
+        list.add("sword");
+        list.add("tiger");
+        return list;
     }
 }
